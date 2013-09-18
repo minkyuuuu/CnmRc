67439ffeba599879ba59821c31ac7e2f7fd854cd
minkyu <minkyuuuu@gmail.com>
2013-09-18 20:46:47 +0900

    StartUpActivity : splash & KeyStore

--- src/com/cnm/cnmrc/MainActivity.java
+++ src/com/cnm/cnmrc/MainActivity.java
@@ -1,15 +1,24 @@
 package com.cnm.cnmrc;
 
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -36,6 +45,11 @@ import com.cnm.cnmrc.fragment.search.SearchVodDetail;
 import com.cnm.cnmrc.fragment.vodtvch.Base;
 import com.cnm.cnmrc.fragment.vodtvch.VodDetail;
 import com.cnm.cnmrc.fragment.vodtvch.VodTvchMain;
+import com.google.android.apps.tvremote.BaseActivity;
+import com.google.android.apps.tvremote.KeyboardActivity;
+import com.google.android.apps.tvremote.layout.SlidingLayout;
+import com.google.android.apps.tvremote.widget.HighlightView;
+import com.google.anymote.Key;
 
 /**
  * 
@@ -152,6 +166,8 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 		//        fm.removeOnBackStackChangedListener(mRemoveOnBackStackChangedListener);
 		//        fm.addOnBackStackChangedListener(mAddOnBackStackChangedListener);
 
+//	    Intent intent = new Intent(this, StartupActivity.class);
+//	    startActivity(intent);
 	}
 
 	@Override
@@ -1068,5 +1084,225 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 	//			//int checkedId = tt.getBreadCrumbTitleRes();
 	//		}
 	//	};
+	
+	
+	
+	  @Override
+	  protected void onDestroy() {
+	    super.onDestroy();
+	  }
+	
+	
+//	  private static final String LOG_TAG = "RemoteActivity";
+//
+//	  private HighlightView surface;
+//
+//	  private final Handler handler;
+//
+//	  /**
+//	   * The enum represents modes of the remote controller with
+//	   * {@link SlidingLayout} screens assignment. In conjunction with
+//	   * {@link ModeSelector} allows sliding between the screens.
+//	   */
+//	  private enum RemoteMode {
+//	    TV(0, R.drawable.icon_04_touchpad_selector),
+//	    TOUCHPAD(1, R.drawable.icon_04_buttons_selector);
+//
+//	    private final int screenId;
+//	    private final int switchButtonId;
+//
+//	    RemoteMode(int screenId, int switchButtonId) {
+//	      this.screenId = screenId;
+//	      this.switchButtonId = switchButtonId;
+//	    }
+//	  }
+//
+//	  /**
+//	   * Mode selector allow sliding across the modes, keeps currently selected mode
+//	   * information, and slides among the modes.
+//	   */
+//	  private static final class ModeSelector {
+//	    private final SlidingLayout slidingLayout;
+//	    private final ImageButton imageButton;
+//	    private RemoteMode mode;
+//
+//	    ModeSelector(
+//	        RemoteMode initialMode, SlidingLayout slidingLayout, ImageButton imageButton) {
+//	      mode = initialMode;
+//
+//	      this.slidingLayout = slidingLayout;
+//	      this.imageButton = imageButton;
+//
+//	      applyMode();
+//	    }
+//
+//	    void slideNext() {
+//	      setMode(RemoteMode.TOUCHPAD.equals(mode) ? RemoteMode.TV : RemoteMode.TOUCHPAD);
+//	    }
+//
+//	    void setMode(RemoteMode newMode) {
+//	      mode = newMode;
+//	      applyMode();
+//	    }
+//
+//	    void applyMode() {
+//	      slidingLayout.snapToScreen(mode.screenId);
+//	      imageButton.setImageResource(mode.switchButtonId);
+//	    }
+//	  }
+//	
+//
+//	
+//	
+//	  public HighlightView getHighlightView() {
+//		    return surface;
+//		  }
+//
+//		  // KeyCode handler implementation.
+//		  public void onRelease(Key.Code keyCode) {
+//		    getCommands().key(keyCode, Key.Action.UP);
+//		  }
+//
+//		  public void onTouch(Key.Code keyCode) {
+//		    playClick();
+//		    getCommands().key(keyCode, Key.Action.DOWN);
+//		  }
+//
+//		  private void playClick() {
+//		    ((AudioManager) getSystemService(Context.AUDIO_SERVICE)).playSoundEffect(
+//		        AudioManager.FX_KEY_CLICK);
+//		  }
+//
+//		  private void flingIntent(Intent intent) {
+//		    if (intent != null) {
+//		      if (Intent.ACTION_SEND.equals(intent.getAction())) {
+//		        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+//		        if (text != null) {
+//		          Uri uri = Uri.parse(text);
+//		          if (uri != null && ("http".equals(uri.getScheme())
+//		              || "https".equals(uri.getScheme()))) {
+//		            getCommands().flingUrl(text);
+//		          } else {
+//		            Toast.makeText(
+//		                this, R.string.error_could_not_send_url, Toast.LENGTH_SHORT)
+//		                .show();
+//		          }
+//		        } else {
+//		          Log.w(LOG_TAG, "No URI to fling");
+//		        }
+//		      }
+//		    }
+//		  }
+//
+//		  @Override
+//		  protected void onKeyboardOpened() {
+//		    showActivity(KeyboardActivity.class);
+//		  }
+//
+//		  // SUBACTIVITIES
+//
+//		  /**
+//		   * The activities that can be launched from the main screen.
+//		   * <p>
+//		   * These codes should not conflict with the request codes defined in
+//		   * {@link BaseActivity}.
+//		   */
+//		  private enum SubActivity {
+//		    VOICE_SEARCH,
+//		    UNKNOWN;
+//
+//		    public int code() {
+//		      return BaseActivity.FIRST_USER_CODE + ordinal();
+//		    }
+//
+//		    public static SubActivity fromCode(int code) {
+//		      for (SubActivity activity : values()) {
+//		        if (code == activity.code()) {
+//		          return activity;
+//		        }
+//		      }
+//		      return UNKNOWN;
+//		    }
+//		  }
+//	
+//	
+//	  @Override
+//	  protected void onActivityResult(
+//	      int requestCode, int resultCode, Intent data) {
+//	    SubActivity activity = SubActivity.fromCode(requestCode);
+//	    switch (activity) {
+//	      case VOICE_SEARCH:
+//	        onVoiceSearchResult(resultCode, data);
+//	        break;
+//
+//	      default:
+//	        super.onActivityResult(requestCode, resultCode, data);
+//	        break;
+//	    }
+//	  }
+//
+//	  // VOICE SEARCH
+//
+//	  private void showVoiceSearchActivity() {
+//	    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//	    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//	        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//	    startActivityForResult(intent, SubActivity.VOICE_SEARCH.code());
+//	  }
+//
+//	  private void onVoiceSearchResult(int resultCode, Intent data) {
+//	    String searchQuery;
+//
+//	    if ((resultCode == RESULT_CANCELED) || (data == null)) {
+//	      return;
+//	    }
+//
+//	    ArrayList<String> queryResults =
+//	        data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//	    if ((queryResults == null) || (queryResults.isEmpty())) {
+//	      Log.d(LOG_TAG, "No results from VoiceSearch server.");
+//	      return;
+//	    } else {
+//	      searchQuery = queryResults.get(0);
+//	      if (TextUtils.isEmpty(searchQuery)) {
+//	        Log.d(LOG_TAG, "Empty result from VoiceSearch server.");
+//	        return;
+//	      }
+//	    }
+//
+//	    showVoiceSearchDialog(searchQuery);
+//	  }
+//
+//	  private void showVoiceSearchDialog(final String query) {
+//	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//	    builder
+//	        .setNeutralButton(
+//	            R.string.voice_send, new DialogInterface.OnClickListener() {
+//	              public void onClick(DialogInterface dialog, int which) {
+//	                getCommands().string(query);
+//	              }
+//	            })
+//	        .setPositiveButton(
+//	            R.string.voice_search_send, new DialogInterface.OnClickListener() {
+//	              public void onClick(DialogInterface dialog, int which) {
+//	                getCommands().keyPress(Key.Code.KEYCODE_SEARCH);
+//	                // Send query delayed
+//	                handler.postDelayed(new Runnable() {
+//	                  public void run() {
+//	                    getCommands().string(query);
+//	                  }
+//	                }, getResources().getInteger(R.integer.search_query_delay));
+//	              }
+//	            })
+//	        .setNegativeButton(
+//	            R.string.pairing_cancel, new DialogInterface.OnClickListener() {
+//	              public void onClick(DialogInterface dialog, int which) {
+//	              }
+//	            })
+//	        .setCancelable(true)
+//	        .setTitle(R.string.voice_dialog_label)
+//	        .setMessage(query);
+//	    builder.create().show();
+//	  }
 
 }
---
+++ src/com/google/android/apps/tvremote/StartupActivity.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2010 Google Inc.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.tvremote;
+
+import java.util.concurrent.TimeUnit;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.cnm.cnmrc.MainActivity;
+import com.cnm.cnmrc.R;
+import com.cnm.cnmrc.util.TimeWatch;
+
+/**
+ * Startup activity that checks if certificates are generated, and if not begins
+ * async generation of certificates, and displays remote logo.
+ * 
+ */
+public class StartupActivity extends CoreServiceActivity {
+
+	private boolean keystoreAvailable;
+	TimeWatch watch;
+
+	/**
+	 * 주 기능:
+	 * splash display (1500 milliseconds)
+	 * make KeyStore at /data/data/com.cnm.cnmrc/files/ipremote.keystore
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Show splash UI.
+		setContentView(R.layout.tutorial);
+		
+		// show MainActiviry after 1500 milliseconds
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				keystoreAvailable = true;
+				showMainActivity();
+			}
+		}, 1500);
+	}
+
+	@Override
+	protected void onServiceAvailable(CoreService coreService) {
+		if (!getKeyStoreManager().hasServerIdentityAlias()) {
+			Log.e("hwang-tvremote", "startupActivity start");
+			watch = TimeWatch.start();
+			new KeystoreInitializerTask(getUniqueId()).execute(getKeyStoreManager());	// take times : 1500 milliseconds
+		} else {
+			Log.i("hwang-tvremote", "already KeyStore hasServerIdentityAlias");
+		}
+	}
+
+	@Override
+	protected void onServiceDisconnecting(CoreService coreService) {
+		// Do nothing
+	}
+	
+	private void showMainActivity() {
+		Intent intent = new Intent(this, MainActivity.class);
+		Intent originalIntent = getIntent();
+		if (originalIntent != null) {
+			intent.setAction(originalIntent.getAction());
+			intent.putExtras(originalIntent);
+		}
+		startActivity(intent);
+	}
+	
+	// No used
+	/*@Override
+	protected void onNewIntent(Intent intent) {
+		setIntent(intent);
+		if (keystoreAvailable) {
+			showMainActivity();
+		}
+	}*/
+
+	private class KeystoreInitializerTask extends AsyncTask<KeyStoreManager, Void, Void> {
+		private final String id;
+
+		public KeystoreInitializerTask(String id) {
+			this.id = id;
+		}
+
+		@Override
+		protected Void doInBackground(KeyStoreManager... keyStoreManagers) {
+			if (keyStoreManagers.length != 1) {
+				throw new IllegalStateException("Only one key store manager expected");
+			}
+			keyStoreManagers[0].initializeKeyStore(id);
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			long elapsedTime = watch.time();
+			//long elapsedTime = watch.time(TimeUnit.SECONDS);
+			Log.e("hwang-tvremote", "elapsedTime : " + elapsedTime);	// 1266, 1455
+			keystoreAvailable = true;
+		}
+	}
+
+	private String getUniqueId() {
+		String id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+		// null ANDROID_ID is possible on emulator
+		return id != null ? id : "emulator";
+	}
+}
